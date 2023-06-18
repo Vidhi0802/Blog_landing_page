93fcac02daaab26cf72a13f01fea77185d035e2d
Vidhi <vidhikulshreshtha.08@gmail.com>
2023-06-18 20:53:20 +0530

    2ndCommit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css';
 
+const pictures = new URL('../img/bg.jpg',import.meta.url)
 
 function App() {
   return (
     <div className='App'> 
       <p>Blogs</p> 
-    
+      <div >
+          
+        <img className='bg' alt='' src={pictures}/>
+        </div>       
     </div>
   );
 }
